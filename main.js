0caf0ff73108af02cd2579dc4f696020cc093317
orxan97 <orkhanna@code.edu.az>
2023-03-24 18:11:44 +0400

    corusel slider

--- main.js
+++ main.js
@@ -1,30 +1,67 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+
+let products=[
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "Men",pic: "./photo/latest1.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "women",pic: "./photo/latest2.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "Men",pic: "./photo/latest1.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "baby",pic: "./photo/latest3.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "Men",pic: "./photo/latest1.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "women",pic: "./photo/latest2.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "baby",pic: "./photo/latest3.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "Men",pic: "./photo/latest1.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "women",pic: "./photo/latest2.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "baby",pic: "./photo/latest3.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "women",pic: "./photo/latest2.jpg",},
+  {  name: "Cashmere Tank+Bag",oldPrice: 120,price: 98,category: "baby",pic: "./photo/latest3.jpg",},
+]
+let swiperWrapper=document.querySelector(".swiper-wrapper");
+products.forEach(product => {
+  swiperWrapper.innerHTML+=`
+     <div class="swiper-slide">
+          <div class="swiper-slide-img">
+
+            <img src="${product.pic}" alt="" />
+            <div class="corusel_item_icons">
+              <i class="fa-solid fa-cart-shopping"></i>
+              <i class="fa-regular fa-heart"></i>
+              <i class="fa-solid fa-magnifying-glass-plus"></i>
+            </div>
+          </div>
+          <div class="slider_corusel_text">
+          <p class="productName">${product.name}</p>
+          <p class="productPrice"> $${product.price} <del>$${product.oldPrice}</del></p>
+        </div>
+        
+        </div>`
+  
+});
+
