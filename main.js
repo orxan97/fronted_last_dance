0b64d0531bd465726319ccd7ba1b9596c306f7d8
orxan97 <orkhanna@code.edu.az>
2023-03-28 17:57:38 +0400

    the end

--- main.js
+++ main.js
@@ -125,7 +125,49 @@ let MayYouLikeProducts = [
     oldPrice: 120,
     price: 98,
 
-    pic: "./photo/latest4.jpg",
+    pic: "./photo/latest5.jpg",
+  },
+  {
+    name: "Cashmere Tank+Bag",
+    oldPrice: 120,
+    price: 98,
+
+    pic: "./photo/latest5.jpg",
+  },
+  {
+    name: "Cashmere Tank+Bag",
+    oldPrice: 120,
+    price: 98,
+
+    pic: "./photo/latest6.jpg",
+  },
+  {
+    name: "Cashmere Tank+Bag",
+    oldPrice: 120,
+    price: 98,
+
+    pic: "./photo/latest6.jpg",
+  },
+  {
+    name: "Cashmere Tank+Bag",
+    oldPrice: 120,
+    price: 98,
+
+    pic: "./photo/latest6.jpg",
+  },
+  {
+    name: "Cashmere Tank+Bag",
+    oldPrice: 120,
+    price: 98,
+
+    pic: "./photo/latest7.jpg",
+  },
+  {
+    name: "Cashmere Tank+Bag",
+    oldPrice: 120,
+    price: 98,
+
+    pic: "./photo/latest7.jpg",
   },
   {
     name: "Cashmere Tank+Bag",
@@ -146,27 +188,23 @@ let MayYouLikeProducts = [
     oldPrice: 120,
     price: 98,
 
-    pic: "./photo/latest1.jpg",
+    pic: "./photo/latest8.jpg",
   },
   {
     name: "Cashmere Tank+Bag",
     oldPrice: 120,
     price: 98,
 
-    pic: "./photo/latest2.jpg",
+    pic: "./photo/latest8.jpg",
   },
   {
     name: "Cashmere Tank+Bag",
     oldPrice: 120,
     price: 98,
-    pic: "./photo/latest3.jpg",
+
+    pic: "./photo/latest8.jpg",
   },
-  {
-    name: "Cashmere Tank+Bag",
-    oldPrice: 120,
-    price: 98,
-    pic: "./photo/latest1.jpg",
-  },
+ 
 ];
 let swiperWrapper = document.querySelector(".swiper-wrapper");
 let MayYouLike = document.querySelector(".wrapper2");
@@ -187,23 +225,27 @@ products.forEach((product) => {
         </div>
         </div>`;
 
+ 
+});
+MayYouLikeProducts.forEach(product => {
   MayYouLike.innerHTML += `
-        <div class="swiper-slide">
-             <div class="swiper-slide-img">
-   
-               <img src="${product.pic}" alt="" />
-               <div class="corusel_item_icons">
-                 <i class="fa-solid fa-cart-shopping"></i>
-                 <i class="fa-regular fa-heart"></i>
-                 <i class="fa-solid fa-magnifying-glass-plus"></i>
-               </div>
-             </div>
-             <div class="slider_corusel_text">
-             <p class="productName">${product.name}</p>
-             <p class="productPrice"> $${product.price} <del>$${product.oldPrice}</del></p>
-           </div>
-           
-           </div>`;
+  <div class="swiper-slide">
+       <div class="swiper-slide-img">
+
+         <img src="${product.pic}" alt="" />
+         <div class="corusel_item_icons">
+           <i class="fa-solid fa-cart-shopping"></i>
+           <i class="fa-regular fa-heart"></i>
+           <i class="fa-solid fa-magnifying-glass-plus"></i>
+         </div>
+       </div>
+       <div class="slider_corusel_text">
+       <p class="productName">${product.name}</p>
+       <p class="productPrice"> $${product.price} <del>$${product.oldPrice}</del></p>
+     </div>
+     
+     </div>`;
+  
 });
 
 let calcScrollValue = () => {
@@ -235,9 +277,21 @@ let search_container = document.querySelector(".search_container");
 let searchClick = document.querySelector(".searchClick");
 let searchX = document.querySelector(".fa-circle-xmark");
 searchClick.addEventListener("click", () => {
-  search_container.style.display = "block";
+  search_container.style.display = "block ";
+
   searchX.style.opacity = "100";
 });
 searchX.addEventListener("click", () => {
   search_container.style.display = "none";
+});
+let about_list_item_blog = document.querySelector(".about_list_item_blog");
+let blog_page = document.querySelector(".blog_page");
+let down = document.querySelector(".down");
+
+about_list_item_blog.addEventListener("click", () => {
+  blog_page.style.display = "block";
+  down.style.opacity ="100";
+});
+down.addEventListener("click", () => {
+  blog_page.style.display = "none";
 });
